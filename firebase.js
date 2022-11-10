6cee9a4c0965342702856354e0a16dec69f46017
Sarah Alli <desdequeensnyc@gmail.com>
2022-11-10 14:11:09 -0500

    updated repo

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+import { initializeApp } from "firebase/app";
+const app = initializeApp(firebaseConfig);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2ORPHhysb2gQyCgeWaofluqulEZJzgjs",
+  authDomain: "music-collaboration-app.firebaseapp.com",
+  projectId: "music-collaboration-app",
+  storageBucket: "music-collaboration-app.appspot.com",
+  messagingSenderId: "764644764614",
+  appId: "1:764644764614:web:20be2714826d7bb09920c7",
+};
