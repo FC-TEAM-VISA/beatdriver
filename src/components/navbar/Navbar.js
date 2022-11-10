6595ca6610a8b79b00801d1a0d33f2d69774262a
Sarah Alli <desdequeensnyc@gmail.com>
2022-11-10 16:29:10 -0500

    routes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <div className="flex items-center space-x-3 p-2 pl-6 bg-blue-400 text-white text-sm">
-      <p className="link">Discover</p>
+      <Link href="/discover">
+        <p className="link">Discover</p>
+      </Link>
       <p className="link">My Projects</p>
       <p className="link">Login</p>
       <p className="link">Sign Up</p>
---
+++ src/pages/discover.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+
+function discover() {
+  return <div>discover this shit</div>;
+}
+
+export default discover;
