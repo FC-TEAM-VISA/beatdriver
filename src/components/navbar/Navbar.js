bbeee5a67fbfa11fb0ae0c0fb14961bbba29c1ea
Sarah Alli <desdequeensnyc@gmail.com>
2022-11-12 20:52:41 -0500

    navbar updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,15 +3,40 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <div className="flex items-center space-x-3 p-2 pl-6 bg-blue-400 text-white text-sm">
-      <Link href="/discover">
-        <p className="link">Discover</p>
-      </Link>
-      <p className="link">My Projects</p>
-      <p className="link">Login</p>
-      <p className="link">Sign Up</p>
-    </div>
+    <header>
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-blue-400 text-white text-sm ">
+        {/* LEFT NAV */}
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0 mr-80">
+          <Link href="/">
+            <h3>BOMBBEATZ</h3>
+          </Link>
+        </div>
+        {/* RIGHT NAV */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap pl-48">
+          {/* <div onClick={!session ? signIn : signOut} className="link">
+            {session ? `Hello, ${session.user.name}!` : "Sign In"}
+            <p className="font-extrabold md:text-sm">Log In</p>
+          </div> */}
+          <Link href="/discover">
+            <p className="link">Discover</p>
+          </Link>
+          <Link href="/board">
+            <p className="link">New Project</p>
+          </Link>
+          <p className="link">Sign In</p>
+        </div>
+      </div>
+    </header>
   );
 }
 
 export default Navbar;
+
+{
+  /* <div className="flex items-center space-x-3 p-2 pl-6 bg-blue-400 text-white text-sm">
+
+<p className="link">My Projects</p>
+<p className="link">Login</p>
+<p className="link">Sign Up</p>
+</div> */
+}
