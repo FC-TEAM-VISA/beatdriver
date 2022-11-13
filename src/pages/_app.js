d21d38975681acec15c34a9ae52c212a4f2e62a0
Sarah Alli <desdequeensnyc@gmail.com>
2022-11-12 20:29:44 -0500

    routing fixed, static nav

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
 import "../../styles/globals.css";
+import Head from "next/head";
+import Navbar from "../components/navbar/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Bombest Beats</title>
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
