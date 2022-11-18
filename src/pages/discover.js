02441b5804a51cf1bb8f831ae9e2e408b4ec4abc
Vi Xiang <vxiang0000@gmail.com>
2022-11-18 11:17:17 -0500

    test

--- src/pages/discover.js
+++ src/pages/discover.js
@@ -1,8 +1,32 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
+import {
+	collection,
+	doc,
+	updateDoc,
+	serverTimestamp,
+	addDoc,
+	setDoc,
+	where,
+	query,
+  ref
+} from "firebase/firestore";
+import { database, storage, auth } from "../../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
-function discover() {
-  return <div>discover</div>;
+const Discover = () => {
+  const q = query(collection(database, 'projects'), where('isPublic', '==', 'true'));
+
+
+	return (
+		<>
+      <Navbar />
+			<div>
+
+      </div>
+		</>
+	);
 }
 
-export default discover;
+export default Discover;
