151038561ea02c75511859e4daf1ac272b78c129
Vi Xiang <vxiang0000@gmail.com>
2022-11-18 19:16:41 -0500

    unignoring

---
+++ src/pages/board/[id].js
@@ -0,0 +1,392 @@
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import * as Tone from "tone";
+import Looper from "../../components/board/looper";
+import AudioPlayer from "../../components/board/audioPlayer";
+import { BsFillPlayFill, BsStopFill } from "react-icons/bs";
+import { BiSave } from "react-icons/bi";
+import SoundMenu from "../../components/soundmenu/SoundMenu";
+import LoadMenu from "../../components/loadmenu/LoadMenu";
+
+//firebase imports
+import {
+	collection,
+	doc,
+	updateDoc,
+	serverTimestamp,
+	addDoc,
+	setDoc,
+	where,
+	query,
+} from "firebase/firestore";
+import { database, auth } from "../../../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { getAllPostIds, getPostData } from "../../../utils/projects";
+// import { GetServerSideProps } from "next";
+
+export async function getStaticPaths() {
+	console.log("this is getStaticPaths");
+	let paths = await getAllPostIds();
+	console.log("paths", paths);
+
+	// const newPaths = paths.map((path) => {
+	// 	params: {
+	// 		id: path.toString();
+	// 	}
+	// });
+
+	let newPaths = [];
+	paths.forEach((path) => {
+		newPaths.push({ params: { id: path.toString() } });
+	});
+	// const pathsWithParams = slugs.map((slug) => ({ params: { postSlug: slug } }));
+	console.log("newPaths", newPaths);
+
+	return {
+		paths: newPaths,
+		fallback: false,
+	};
+}
+
+export async function getStaticProps({ params }) {
+	console.log("this is getStaticProps");
+	const data = JSON.stringify(await getPostData(params.id));
+	console.log("THIS IS DATA IN PROPS", data);
+
+	return {
+		props: {
+			data,
+		},
+	};
+}
+
+// export const getServerSideProps = async () => {
+//   const isPublicQuery = query(
+//     collection(database, "projects"),
+//     where("isPublic", "==", true)
+//   );
+//   const querySnapshot = await getDocs(isPublicQuery);
+// 	const data = querySnapshot.docs.map((doc) => doc.data());
+//   if (!data) return { notFound: true };
+//   return { props: { data } };
+// };
+
+// export async function getServerSideProps(context) {
+//   const isPublicQuery = query(
+//     collection(database, "projects"),
+//     where("isPublic", "==", true)
+//   );
+//   const querySnapshot = await getDocs(isPublicQuery);
+// 	const docsData = querySnapshot.docs.map((doc) => ({id: doc.id, data: doc.data()}));
+//   return {
+//     props: { data: JSON.parse(JSON.stringify(docsData)) },
+//   }
+// }
+
+// export async function getServerSideProps(context) {
+// 	try {
+// 		// const cookies = nookies.get(context);
+// 		// const token = await verifyIdToken(cookies.token);
+// 		// const { email } = token;
+// 		// const fs = require("fs/promises");
+
+// 		const q = query(
+// 			collection(database, "projects"),
+// 			where("isPublic", "==", true),
+// 			orderBy("updatedAt")
+// 		);
+// 		const querySnapshot = await collection(
+// 			database,
+// 			"projects",
+// 			context.query.projectId
+// 		).get();
+
+// 		let todos = [];
+// 		querySnapshot.forEach((doc) => {
+// 			todos.push({
+// 				...doc.data(),
+// 				id: doc.id,
+// 				updatedAt: doc.data().updatedAt.toDate().getTime(),
+// 			});
+// 		});
+
+// 		console.log("THIS IS TODOSSS", todos);
+
+// 		return {
+// 			props: {
+// 				data: JSON.stringify(todos) || [],
+// 			},
+// 			// props: { session: `Your email is ${email} and your UID is ${uid}.` },
+// 		};
+// 	} catch (error) {
+// 		return { props: {} };
+// 	}
+// }
+
+/* THE BOARD*/
+const steps = 8;
+const buttonState = { triggered: false, activated: false, audio: "" };
+const sounds = [
+	["1", "2", "3", "4", "5", "6", "7", "8"],
+	["9", "10", "11", "12", "13", "14", "15", "16"],
+	["17", "18", "19", "20", "21", "22", "23", "24"],
+	["25", "26", "27", "28", "29", "30", "31", "32"],
+	["33", "34", "35", "36", "37", "38", "39", "40"],
+];
+
+//sets up how big the grid will be
+const initialGrid = [
+	new Array(8).fill(buttonState),
+	new Array(8).fill(buttonState),
+	new Array(8).fill(buttonState),
+	new Array(8).fill(buttonState),
+	new Array(8).fill(buttonState),
+];
+
+const Board = ({ data }) => {
+	console.log("THIS IS DATAAAAAAAA", data);
+
+	const [user] = useAuthState(auth);
+	const [isPublic, setIsPublic] = useState(true);
+	const [beat, setBeat] = useState("./samples/drums/clap-808.wav");
+	const [bpm, setBpm] = useState(120);
+	const [uniqueID, setUniqueID] = useState(null);
+	const [playing, setPlaying] = useState(false);
+	const [objectSounds, setObjectSounds] = useState({
+		"./samples/drums/clap-808.wav": "./samples/drums/clap-808.wav",
+	});
+	const [grid, setGrid] = useState(initialGrid);
+	const [userGoogleInfo] = useAuthState(auth);
+	// console.log("GoogleInfo: ", userGoogleInfo);
+
+	const dbRef = collection(database, "users");
+	const [docs] = useCollectionData(dbRef);
+
+	let currentUser;
+	if (user) {
+		currentUser = docs?.find((doc) => doc.email === user.email);
+	}
+	console.log("USER", currentUser);
+
+	const dbInstance = query(
+		collection(database, "projects"),
+		where(`ownerId`, "==", `${userGoogleInfo?.uid}`)
+	);
+
+	console.log("IWORK: ", dbInstance);
+	// const dbInstance = collection(database, "projects");
+
+	const [projects] = useCollectionData(dbInstance);
+
+	// let currentProject = projects?.find(
+	//   (project) => user.email === project.ownerId
+	// );
+	// console.log("current", currentProject);
+
+	console.log("I AM A PROJECT: ", projects);
+	console.log(uniqueID);
+
+	const handleSave = async () => {
+		if (!uniqueID) {
+			const newProject = await addDoc(collection(database, `projects`), {
+				createdAt: serverTimestamp(),
+				ownerId: user.uid,
+				ownerName: user.name,
+				name: "Untitled",
+				grid: {
+					r1: grid[0],
+					r2: grid[1],
+					r3: grid[2],
+					r4: grid[3],
+					r5: grid[4],
+				},
+				bpm: +bpm,
+				isPublic: true,
+			});
+			setUniqueID(newProject.id);
+
+			await setDoc(
+				doc(database, `projects/${newProject.id}`),
+				{
+					projectId: newProject.id,
+				},
+				{ merge: true }
+			);
+		} else {
+			// setBpm(data.bpm);
+			// setGrid(data.grid);
+			// setIsPublic(postData.isPublic);
+
+			await updateDoc(doc(database, `projects/${uniqueID}`), {
+				updatedAt: serverTimestamp(),
+				grid: {
+					r1: grid[0],
+					r2: grid[1],
+					r3: grid[2],
+					r4: grid[3],
+					r5: grid[4],
+				},
+				bpm: +bpm,
+				isPublic,
+			});
+
+			setUniqueID(uniqueID);
+		}
+	};
+
+	// KEEP THIS FOR TESTING COLLABORATION
+	// useEffect(() => {
+	//   if (uniqueID) {
+	//     const realTime = async () => {
+	//       await updateDoc(doc(database, `projects/${uniqueID}`), {
+	//         createdAt: serverTimestamp(),
+	//         grid: {
+	//           r1: grid[0],
+	//           r2: grid[1],
+	//           r3: grid[2],
+	//           r4: grid[3],
+	//           r5: grid[4],
+	//         },
+	//         bpm: +bpm,
+	//       });
+	//     };
+	//     realTime();
+	//   }
+	// }, [grid, bpm]);
+
+	const handleBeatChange = (e) => {
+		if (!objectSounds[e.target.value]) {
+			let copyObject = { ...objectSounds };
+			copyObject[e.target.value] = e.target.value;
+			setObjectSounds(copyObject);
+		}
+		setBeat(e.target.value);
+	};
+
+	return (
+		<>
+			<div className="grid grid-cols-12 text-xl">
+				{/* TOOLBAR */}
+				<div className="flex flex-grow col-span-9 bg-teal-800">
+					<div className="flex bg-teal-800 ml-3">
+						<button
+							onClick={() => {
+								setPlaying(!playing);
+								Tone.start();
+							}}
+						>
+							{playing ? (
+								<BsStopFill className="text-white bg-teal-800 h-12 w-12 p-2" />
+							) : (
+								<BsFillPlayFill className="text-white bg-teal-800 h-12 w-12 p-2" />
+							)}
+						</button>
+					</div>
+					<div>
+						<BiSave
+							className="mt-4 mr-3 ml-2 cursor-pointer"
+							onClick={() =>
+								user
+									? handleSave()
+									: window.alert("LOG IN OR SIGN UP TO SAVE A PROJECT")
+							}
+						/>
+					</div>
+					<LoadMenu
+						projects={projects}
+						setGrid={setGrid}
+						setUniqueID={setUniqueID}
+						uniqueID={uniqueID}
+					/>
+					<div>
+						<button
+							onClick={() => {
+								setGrid(initialGrid);
+								setObjectSounds({
+									"./samples/drums/clap-808.wav":
+										"./samples/drums/clap-808.wav",
+								});
+							}}
+							className="mt-1 mx-2 border-2 p-1 bg-red-900 hover:bg-red-600 border-white"
+						>
+							CLEAR BOARD
+						</button>
+					</div>
+					<div className="p-2">
+						{/* DROPDOWN */}
+						<label className="p-2">BEAT:</label>
+						<select
+							className="p-1"
+							name="beat"
+							onChange={(e) => {
+								handleBeatChange(e);
+							}}
+						>
+							<option value="./samples/drums/clap-808.wav">clap-808</option>
+							<option value="./samples/drums/clap-analog.wav">
+								clap-analog
+							</option>
+							<option value="./samples/drums/clap-crushed.wav">
+								clap-crushed
+							</option>
+						</select>
+					</div>
+
+					<div className="p-2 mx-4 mt-1">
+						<label className="pr-2">SOUNDS:</label>
+						<SoundMenu beat={beat} handleBeatChange={handleBeatChange} />
+					</div>
+
+					<div className="p-2">
+						{/* BPM */}
+						<label className="p-2">BPM:</label>
+						<input
+							type="range"
+							min="50"
+							max="300"
+							onChange={(e) => setBpm(e.target.value)}
+						/>
+						<output className="p-1">{bpm}</output>
+					</div>
+				</div>
+
+				<div className="col-span-9">
+					<AudioPlayer objectSounds={objectSounds} bpm={bpm}>
+						{({ player }) => {
+							if (!player) {
+								return (
+									<p className="flex items-center justify-center animate-bounce">
+										LOADING....
+									</p>
+								);
+							}
+							return (
+								<Looper
+									player={player}
+									bpm={bpm}
+									playing={playing}
+									beat={beat}
+									objectSounds={objectSounds}
+									steps={steps}
+									sounds={sounds}
+									grid={grid}
+									setGrid={setGrid}
+									uniqueID={uniqueID}
+									handleSave={handleSave}
+								/>
+							);
+						}}
+					</AudioPlayer>
+				</div>
+
+				<div className="col-span-3">
+					<div className="bg-blue-200 h-full col-span-2">
+						<div className=" bg-purple-400"></div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Board;
