36a7af3c1a1ee2c0eb07d8654f041f8bb48f24df
Vi Xiang <vxiang0000@gmail.com>
2022-11-22 21:11:49 -0500

    removed commaaaa

--- src/components/board/Grid.js
+++ src/components/board/Grid.js
@@ -2,29 +2,29 @@ import React from "react";
 import Button from "./Button";
 
 const Grid = ({ grid, toggleActivation, selectedInstrument }) => {
-  return (
-    <div className="flex items-center my-2">
-      <div className="flex-1 max-w-4xl mx-auto p-5">
-        <ul className="grid grid-cols-8 grid-rows-5 gap-8">
-          {grid.map((line, row) =>
-            line.map((time, col) => {
-              return (
-                <Button
-                  key={row + col}
-                  column={col + 1}
-                  row={row + 1}
-                  activated={grid[row][col]["activated"]}
-                  triggered={grid[row][col]["triggered"]}
-                  toggleActivation={() => toggleActivation(row, col)}
-                  selectedInstrument={selectedInstrument}
-                />
-              );
-            })
-          )}
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex items-center my-2">
+			<div className="flex-1 max-w-4xl mx-auto p-5">
+				<ul className="grid grid-cols-8 grid-rows-5 gap-8">
+					{grid.map((line, row) =>
+						line.map((time, col) => {
+							return (
+								<Button
+									key={row + col}
+									column={col + 1}
+									row={row + 1}
+									activated={grid[row][col]["activated"]}
+									triggered={grid[row][col]["triggered"]}
+									toggleActivation={() => toggleActivation(row, col)}
+									selectedInstrument={selectedInstrument}
+								/>
+							);
+						})
+					)}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Grid;
--- src/components/board/Looper.js
+++ src/components/board/Looper.js
@@ -4,87 +4,86 @@ import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 
 const Looper = ({
-  player,
-  bpm,
-  playing,
-  beat,
-  objectSounds,
-  grid,
-  setGrid,
-  steps,
-  selectedInstrument,
-  selected,
-  masterVolume,
+	player,
+	bpm,
+	playing,
+	beat,
+	objectSounds,
+	grid,
+	setGrid,
+	steps,
+	selectedInstrument,
+	selected,
+	masterVolume,
 }) => {
-  const [currButton, setCurrButton] = useState(0);
-  const [open, setOpen] = useState(false);
-  const closeModal = () => setOpen(false);
+	const [currButton, setCurrButton] = useState(0);
+	const [open, setOpen] = useState(false);
+	const closeModal = () => setOpen(false);
 
-  const toggleActivation = (row, col) => {
-    if (selected === "SELECTED") {
-      setOpen(true);
-    } else {
-      const gridCopy = [...grid];
-      const { triggered, activated } = gridCopy[row][col];
-      gridCopy[row][col] = { triggered, activated: !activated, audio: beat };
-      setGrid(gridCopy);
-    }
-  };
+	const toggleActivation = (row, col) => {
+		if (selected === "SELECTED") {
+			setOpen(true);
+		} else {
+			const gridCopy = [...grid];
+			const { triggered, activated } = gridCopy[row][col];
+			gridCopy[row][col] = { triggered, activated: !activated, audio: beat };
+			setGrid(gridCopy);
+		}
+	};
 
-  player.volume.value = masterVolume;
+	player.volume.value = masterVolume;
 
-  //this is what goes through the loop and triggers each row
-  //if a button is triggered and already activated (by user) then it plays the sample
-  const nextButton = (currButton) => {
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[i].length; j++) {
-        const { activated, audio } = grid[i][j];
-        grid[i][j] = { activated, triggered: j === currButton, audio };
+	//this is what goes through the loop and triggers each row
+	//if a button is triggered and already activated (by user) then it plays the sample
+	const nextButton = (currButton) => {
+		for (let i = 0; i < grid.length; i++) {
+			for (let j = 0; j < grid[i].length; j++) {
+				const { activated, audio } = grid[i][j];
+				grid[i][j] = { activated, triggered: j === currButton, audio };
 
-        if (grid[i][j].triggered && grid[i][j].activated && grid[i][j].audio) {
-          //plays the sound associated with the button
-          player.player(objectSounds[grid[i][j].audio]).start();
-        }
-      }
-    }
-    setGrid(grid);
-  };
+				if (grid[i][j].triggered && grid[i][j].activated && grid[i][j].audio) {
+					//plays the sound associated with the button
+					player.player(objectSounds[grid[i][j].audio]).start();
+				}
+			}
+		}
+		setGrid(grid);
+	};
 
-  //timer of the loop, sets what rows are triggered
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (playing) {
-        setCurrButton((currButton + 1) % steps);
-        nextButton(currButton);
-      }
-      //use line below to control speed of timer/works like tempo!
-    }, 60000 / bpm); //(60,000 / bpm = milliseconds for 1/4 notes)
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [currButton, playing]);
+	//timer of the loop, sets what rows are triggered
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (playing) {
+				setCurrButton((currButton + 1) % steps);
+				nextButton(currButton);
+			}
+			//use line below to control speed of timer/works like tempo!
+		}, 60000 / bpm); //(60,000 / bpm = milliseconds for 1/4 notes)
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [currButton, playing]);
 
-  return (
-    <div className="">
-      <Grid
-        grid={grid}
-        toggleActivation={toggleActivation}
-        selectedInstrument={selectedInstrument}
-      />
-      <Popup
-        open={open}
-        closeOnDocumentClick
-        onClose={closeModal}
-        className="popup-content"
-      >
-        <div className="grid bg-oxford_blue place-items-center">
-          <p className="text-4xl mt-10 mb-5">PLEASE SELECT A SOUND!</p>
-          <p className="mb-10">click anywhere to close</p>
-        </div>
-      </Popup>
-      ;
-    </div>
-  );
+	return (
+		<div className="">
+			<Grid
+				grid={grid}
+				toggleActivation={toggleActivation}
+				selectedInstrument={selectedInstrument}
+			/>
+			<Popup
+				open={open}
+				closeOnDocumentClick
+				onClose={closeModal}
+				className="popup-content"
+			>
+				<div className="grid bg-oxford_blue place-items-center">
+					<p className="text-4xl mt-10 mb-5">PLEASE SELECT A SOUND!</p>
+					<p className="mb-10">click anywhere to close</p>
+				</div>
+			</Popup>
+		</div>
+	);
 };
 
 export default Looper;
